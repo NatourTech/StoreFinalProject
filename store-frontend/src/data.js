299e7476c0beb6302398cddb0c9eacf036a070a2
NatourTech <developer1pro@gmail.com>
2022-09-17 10:42:07 +0300

    start data modling

---
+++ store-frontend/src/data.js
@@ -0,0 +1,21 @@
+const data = {
+    
+  products: [
+    {
+      name: "CLASSERICA",
+      category: "office",
+      price: 60,
+      image: "/image.png",
+      description: "good product",
+      rating: 4.7,
+    },
+    {
+      name: " TOUCH יומן יומי 15/21מדורג מעורב צבעים ",
+      category: "office",
+      price: 60,
+      image: "/image.png",
+      description: "good product",
+      rating: 4.7,
+    },
+  ],
+};
