568514f5e1eb4621a760d5d3ac5ae5d706cc34be
Moataz <moatazw72@gmail.com>
2022-09-29 18:00:17 +0300

    replace the use state with the use reducer

--- store-frontend/src/screens/ProductScreen.js
+++ store-frontend/src/screens/ProductScreen.js
@@ -1,14 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useReducer } from 'react';
 //import data from '../data';
 import axios from 'axios';
+import logger from 'use-reducer-logger';
 import { Link } from 'react-router-dom';
 
 function ProductScreen() {
-  const [products, setProducts] = useState([]);
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'FETCH_REQUEST':
+        return { ...state, loading: true };
+      case 'FETCH_SUCCESS':
+        return { ...state, products: action.payload, loading: false }; //payload is contain all the data from the backend
+      case 'FETCH_FAIL':
+        return { ...state, loading: false, error: action.payload };
+
+      default:
+        return state;
+    }
+  };
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: '',
+  });
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get('/api/products');
-      setProducts(result.data);
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
+
+      //setProducts(result.data);
     };
     fetchData();
   }, []);
