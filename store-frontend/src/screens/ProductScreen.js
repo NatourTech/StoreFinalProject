6ea3323c9cac2d90ca9507c966c98ccbe5d4fa71
NatourTech <63518160+NatourTech@users.noreply.github.com>
2022-10-13 17:39:40 +0300

    Update ProductScreen.js

--- store-frontend/src/screens/ProductScreen.js
+++ store-frontend/src/screens/ProductScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer} from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import logger from "use-reducer-logger";
